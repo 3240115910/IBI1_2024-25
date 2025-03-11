69fb50d8af0af8ac9e4dcbb8d227a30c12833be1
3240115910 <jiarui2.24@intl.zju.edu.cn>
2025-03-11 15:59:55 +0800

    mystery_code.py

---
+++ Practical4/mystery_code.py
@@ -0,0 +1,27 @@
+# What does this piece of code do?
+# Answer: To check the two random numbers which are from 1 to 6 how many times can be equal. 
+          
+# Import libraries
+# randint allows drawing a random number,
+# e.g. randint(1,5) draws a number between 1 and 5
+from random import randint
+
+# ceil takes the ceiling of a number, i.e. the next higher integer.
+# e.g. ceil(4.2)=5
+from math import ceil
+# Initialize a variable named progress.
+progress=0
+# Add a cycle that is infinite because progress is non negative.
+while progress>=0:
+	# Add one after each cycle.
+	progress+=1
+	# The first number
+	first_n = randint(1,6)
+    # The second number
+	second_n = randint(1,6)
+	# Add printing restrictive condition
+	if first_n == second_n:
+		# Print the number of times of the cycle.
+		print(progress)
+		break
+
