26a717145c7729b3133d54db60475d6a8a6b418a
3240115910 <jiarui2.24@intl.zju.edu.cn>
2025-03-11 15:29:34 +0800

    triangle.py

---
+++ Practical4/triangle.py
@@ -0,0 +1,8 @@
+# print the first ten numbers
+print("The first ten triangular numbers are:")
+# calculate the numbers
+# from 1 to 10
+for n in range(1,11):
+    triangular_numbers=n*(n+1)//2
+    #print the results of the calculation
+    print(triangular_numbers)
