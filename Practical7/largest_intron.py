dcbd14675c8833844ab7de9faaf558d3c54cb726
3240115910 <jiarui2.24@intl.zju.edu.cn>
2025-04-02 15:54:53 +0800

    Practtical7

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,18 +1,7 @@
 import re
-def intron (seq):
-    GT_site = [m.start() for m in re.finditer('GT', seq)]
-    AG_site = [m.start() for m in re.finditer('AG', seq)]
-    largest = 0
-    largest_intron = ""
-    for GT in GT_site:
-        for AG in AG_site:
-             if AG > GT:
-                l= AG-GT-2
-                if l >largest:
-                    largest = l 
-                    largest_intron = seq[GT + 2:AG]  
-    return largest , largest_intron          
+        
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
-largest_intron_length, largest_intron = intron(seq)
-print (f'The largest intron is {largest_intron}.')
+sequence = re.findall(r'GT.+AG',seq)
+largest_intron_length = [len(intron)for intron in sequence]
+print (f'The largest intron is {sequence}.')
 print (f'The length of largest intron is {largest_intron_length}.')
--- Practical7/tata_box.py
+++ Practical7/tata_box.py
@@ -1,16 +1,22 @@
 import re
+# To start a function.
 def tata_genes(file_path):
+    #Create an empty list
+    #To research the TATAWAW
+    #To tell the computer the position of the file
     gene_results = []
-    tata_pattern = re.compile(r'TATA')
+    tata_pattern = re.compile(r'TATA[AT]A[AT]')
     file_path = r"C:\Users\张嘉芮1\Downloads\Saccharomyces_cerevisiae.R64-1-1.cdna.all.fa"
+    #read the file line by line   
     with open(file_path, 'r') as file:
         gene = None
         seq = []
         for line in file:
             line = line.strip()
-            if line.startswith('>'):
+            if line.startswith('>'):#To make sure whether it is a new gene.
                 if gene:
                     gene_results.append((gene, ''.join(seq)))
+                #To output the results which have name and the sequence.
                 parts = line[1:].split()
                 gene = parts[0]
                 gene = re.sub(r'_mRNA$', '', gene)
@@ -20,12 +26,13 @@ def tata_genes(file_path):
         if gene:
             gene_results.append((gene, ''.join(seq)))    
     tata_genes = []
+    #Screening genes containing TATAWAW
     for gene, seq in gene_results:
-        if any(tata_pattern.search(seq) for _ in range(len(seq))):
+         if tata_pattern.search(seq):
             tata_genes.append([gene, seq])
     
     return tata_genes
-
+# write new FASTA file.
 def write_tata_genes(tata_genes, output_file):
     with open(output_file, 'w') as file:
         for gene,seq in tata_genes:
