4f72c85f589859f6fd5f179e95458d98869319f3
3240115910 <jiarui2.24@intl.zju.edu.cn>
2025-05-21 15:32:59 +0800

    Practical7

--- Practical7/largest_intron.py
+++ Practical7/largest_intron.py
@@ -1,7 +1,10 @@
 import re
-        
+# input the sequence
 seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
+# find the largest intron
 sequence = re.findall(r'GT.+AG',seq)
+# find the lengthe of the largest intron
 largest_intron_length = [len(intron)for intron in sequence]
+#print the results.
 print (f'The largest intron is {sequence}.')
 print (f'The length of largest intron is {largest_intron_length}.')
